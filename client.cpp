11bd271c2c11b46d81af5ced40d764ccc1ac0a97
Jorge <jorge@Nathans-MacBook-Pro-4.local>
2019-04-29 16:36:02 -0700

    Sending info to server

--- client.cpp
+++ client.cpp
@@ -1,24 +1,76 @@
 /* PORT NUMBER 8012 8013*/
 #include <iostream>
 #include <curses.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
 #include "lib/getChar.hpp"
+#define PORT_NUMBER 24
+#define MAXLINE 200
 
 void startup   (void);
 void terminate (void);
 void inBounds  (int &, int &);
+int written(int , char *, int);
 
-int main(void) {
+int main(int argc, char **argv) {
+	/* Create curses */
 	int x,y = 0;
 	startup();    /* Initilize Program */
 	move(x,y);    /* Move the curser to the top and left most block*/
 	refresh();    /* Refresh the screen */
+	/* end of create curses */
 
-	while(true) {
-		mvaddch(y,x, get_char());
-		inBounds(x,y);
-		refresh();
+	/* Create client to server connection */
+	int sockFD, n;
+	char recvline[MAXLINE + 1];
+	struct sockaddr_in servaddr;
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s UserName\n", argv[0]);	
+		exit(1);
+	}
+	// create socket end point fo communcation. Using IPv4 format and stream socket
+	if ((sockFD = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
+		fprintf(stderr, "Socket Error. %s\n", strerror(errno));
+		exit(1);
 	}
 
+	//build a profile for the server 
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port   = htons(PORT_NUMBER);
+	if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
+		fprintf(stderr, "inet ptron failed. %s\n", argv[1]);
+		exit(1);
+	}
+	
+	//build a connection to the server
+	if (connect(sockFD, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0 ) {
+		fprintf(stderr, "Connect error: %s\n", strerror(errno));
+		exit(1);
+	}
+	
+	/* end of creatin connections */
+	char c;
+	while(true) {
+		c = get_char();
+		mvaddch(y,x,c);
+		inBounds(x,y);
+		refresh();
+		/* send character to the server*/
+		if ((written(sockFD, (char *)&c, sizeof(c))) < 0) {
+			fprintf(stderr, "Failed to send character to server\n");
+			exit(0);
+		}
+	}
 	terminate();  /* Terminate Program */
 }
 
@@ -42,5 +94,18 @@ void terminate(void) {
 	clear();
 	refresh();
 	endwin();
+}
+
+int written(int fd, char *c, int size) {
+	int no_left, no_written;
+	no_left = size;
+	while ( no_left > 0) {
+		no_written = write(fd, c, no_left);
+		if ( no_written <= 0) 
+			return no_written;
+		no_left -= no_written;
+		c += no_written;
+	}
+	return size - no_left;
 }
 
